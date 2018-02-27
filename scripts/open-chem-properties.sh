8092afc66ff296402614b9742774f6b3da75194b
Rudraksh_DO <rudrakshtuwani@gmail.com>
2018-02-27 07:24:04 +0000

    setup updated

--- scripts/open-chem-properties.sh
+++ scripts/open-chem-properties.sh
@@ -1,10 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-conda create -n chemopy python=2.7.12 
+########################## SETUP INSTRUCTIONS ################################
+#                                                                            #
+# 1. Make the script executable by running: chmod +x open-chem-properties.sh #
+# 2. Run the following command: ./open-chem-properties.sh                    #
+# 3. Launch jupyter notebook and change kernel to chemopy                    #
+#                                                                            #
+##############################################################################
+
+conda create -n chemopy python=2.7.12 --yes
 source activate chemopy
-conda install -c openbabel openbabel
-conda install -c rdkit rdkit
-wget chemopy-1.0.tar.gz
+conda install -c openbabel openbabel --yes
+conda install -c rdkit rdkit --yes
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/pychem/chemopy-1.0.tar.gz
 tar xzf chemopy-1.0.tar.gz
 cd chemopy-1.0
 python setup.py install
+rm -rf chemopy*
+conda install ipykernel --yes
+python -m ipykernel install --name chemopy --user
